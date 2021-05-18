39781b17818a2ef26d842adec3d6ee87a73aa9be
Javier Villalobos <javiervbk@gmail.com>
2021-05-18 14:23:00 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Plataforma de informarción de Museos"/>
   </div>
 </template>
 
